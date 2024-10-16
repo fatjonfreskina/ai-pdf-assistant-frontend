420546df200810ba1fc55e255666885956de4737
Fatjon Freskina <91606088+fatjonfreskina@users.noreply.github.com>
2024-10-16 22:47:22 +0200

    Refactored index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import WelcomeView from '../views/WelcomeView.vue'
 import { useAuthStore } from '@/stores/AuthStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/welcome',
-      name: 'welcome',
-      component: WelcomeView
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
     },
 
     {
       path: '/home',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
 
-    { path: '/:pathMatch(.*)*', redirect: '/welcome' }
+    { path: '/:pathMatch(.*)*', redirect: '/login' }
   ]
 })
 
 router.beforeEach((to) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/welcome']
+  const publicPages = ['/login', '/register']
   const authRequired = !publicPages.includes(to.path)
   const authStore = useAuthStore()
   if (authRequired && !authStore.user) {
-    console.log('Redirect to welcome')
     return {
-      path: '/welcome',
+      path: '/login',
       query: { returnUrl: to.path }
     }
   }
